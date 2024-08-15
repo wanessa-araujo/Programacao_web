82956734f0c76300d95266213b3942af01330622
Wanessa Araujo <62186456+wanessa-araujo@users.noreply.github.com>
2024-08-15 16:38:26 -0300

    Update app.js

--- jogo-numero-secreto-logica-js-projeto-dois/app.js
+++ jogo-numero-secreto-logica-js-projeto-dois/app.js
@@ -1,22 +1,52 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+function exibirMensagemInicial(){
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+} 
+exibirMensagemInicial();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas >1 ? 'tentavivas' : 'tentativas';
+        let mensagemTentativa = `Você descobriu o numero secreto com ${tentativas} ${palavraTentativa} tentativas!`;
+        exibirTextoNaTela('p', mensagemTentativa);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else{
+            if (chute > numeroSecreto) {
+                exibirTextoNaTela('p', 'O numero secreto é menor');
+            } else {
+                exibirTextoNaTela('p', 'O numero secreto é maior');
+            } 
+            tentativas++;   
+            limparCampo();       
+        }
 }
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
 }
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
 
+}
+function reiniciarJogo(){
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disebled',true)
+}
 
 
 
